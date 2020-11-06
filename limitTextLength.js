ebcc9eb6c1e4ebd492d3705a543a4958c619bf1c
liujiao <864570332@qq.com>
2020-11-06 14:46:57 +0800

    upd  length

--- limitTextLength.js
+++ limitTextLength.js
@@ -1,24 +1,33 @@
 /**
- * 中英文输入字符分别计算 (常用在输入框)
+ * 中英文输入字符长度分别计算
+ * @params value 需要验证的字符串
+ * @return {number} 长度
+ */
+export function calcTextLength(value){
+	if(value===null||value===undefined) return 0;
+	//中文、中文标点、全角字符按 2 长度，英文、英文符号、数字按 1 长度计算
+	let cnReg = /([\u4e00-\u9fa5]|[\u3000-\u303F]|[\uFF00-\uFF60])/g;
+	let mat = value.match(cnReg);
+	if (mat) {
+		return mat.length * 2 + (value.length - mat.length);
+	} else {
+		return value.length;
+	}
+}
+/**
+ * 限制输入长度 （区分中英文）
  * @params value 需要验证的字符串
  * @params len 需要限制的长度
  */
 export function limitTextLength(value,len) {
-	if(value===null||value===undefined) return;
-	// 先粗略判断长度，如果不超过直接返回，感觉会少点计算？
-	if(len >= value.length) return value;
-	//中文、中文标点、全角字符按1长度，英文、英文符号、数字按0.5长度计算
+	if(value===null||value===undefined) return '';
+	let textLen = calcTextLength(value)
 	let cnReg = /([\u4e00-\u9fa5]|[\u3000-\u303F]|[\uFF00-\uFF60])/g;
-	let mat = value.match(cnReg);
-	let textLen;
-	if (mat) {
-		textLen = mat.length + (value.length - mat.length) * 0.5;
-	} else {
-		textLen = value.length * 0.5;
-	}
 	while (textLen > len) {
+		const mat = value.charAt(value.length-1).match(cnReg);
 		value = value.substr(0, value.length - 1);
-		textLen--;
+		if(mat) textLen -=2;
+		else textLen--;
 	}
 	return value;
 }
